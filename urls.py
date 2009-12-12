4d2ca121681610624f0c29c83641d99785006742
Владимир Прохода <vladimirbright@gmail.com>
2009-12-12 22:51:28 +0300

    Шаблон.

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf8 -*-
-
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
+
+from django.conf import settings
 
 from knowledge.cards import views as card_view
 from knowledge.users import views as users_view
@@ -11,6 +12,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', card_view.index),
+    # TODO не забыть убрать обработку статики с джанги.
+    (r'^s/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+
     (r'^cards/', include('knowledge.cards.urls')),
     (r'^login/', login),
     (r'^logout/', logout),
