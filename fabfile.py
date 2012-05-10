2f1d7bf93215b9877d779242e8d6aad3f0937f16
Владимир Прохода <vladimirbright@gmail.com>
2012-05-10 21:42:16 +0400

    remove django-sphinx

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,15 @@
 
 from fabric.api import cd, run, hosts
 
+HOME_DIR = '/home/knbase.org'
+CODE_DIR = '/home/knbase.org/www'
+ENV_DIR = '/home/knbase.org/Env'
+
+PYTHON = u'%s/bin/python %s/manage.py ' %(ENV_DIR, CODE_DIR)
+
 
 @hosts('knbase.org@knbase.org')
-def gp():
-    with cd('/home/knbase.org/www'):
+def git_pull():
+    with cd(CODE_DIR):
         run('git pull', pty=True)
 
